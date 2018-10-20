ab0057add6c5a14b71065470ae3e9daf96ef564e
Milos Berka <milos.berka@gmail.com>
2018-10-20 16:14:05 +0300

    changes to exports trying to exclude react and react-native

--- index.js
+++ index.js
@@ -3,5 +3,26 @@ import { colors } from './assets'
 
 module.exports = {
   Heading: Heading,
-  colors: colors
+  colors: colors,
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-native')
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-native',
+      commonjs2: 'react-native',
+      amd: 'ReactNative',
+      root: 'ReactNative'
+    }
+  }
 }
